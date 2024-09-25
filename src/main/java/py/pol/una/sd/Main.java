0e16e967a85a0661d92f0e050ee7af2a4b6416f0
Marcelo <elias4845@gmail.com>
2024-09-25 17:48:22 -0400

    cada cliente tiene un color diferente al enviar mensajes

--- src/main/java/py/pol/una/sd/Main.java
+++ src/main/java/py/pol/una/sd/Main.java
@@ -1,53 +1,46 @@
 package py.pol.una.sd;
 
 import javax.swing.*;
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
+        int serverPort = 1234;
+        byte[] buffer = new byte[1024];
+        String user = JOptionPane.showInputDialog("Ingrese su nombre de usuario: ");
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                int PUERTO_SERVIDOR = 1234;
-                String user = "";
-                user = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
-                byte[] buffer = new byte[1024];
-                System.out.println(user);
+        try {
+            // Primero enviamos al servidor el nombre de usuario del cliente
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getLocalHost();
+            buffer = user.getBytes(StandardCharsets.UTF_8);
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+            socket.send(packet);
+            // una vez enviado el nombre de usuario, creamos la interfaz y esperamos a recibir mensajes
+            ClientGUI client = new ClientGUI();
+            JFrame frame = new JFrame("ClientGUI");
+            frame.setContentPane(client.getContentPane());
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setSize(800, 600);
+            frame.setVisible(true);
 
-                try {
-                    DatagramSocket socketUDP = new DatagramSocket();
-                    InetAddress server = InetAddress.getByName("localhost");
-
-                    buffer = user.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server, PUERTO_SERVIDOR);
-                    socketUDP.send(packet);
-
-                    ClientGUI client = new ClientGUI();
-                    client.setVisible(true);
-
-                    while (true) {
-                        buffer = new byte[1024];
-                        //packet de escucha
-                        packet = new DatagramPacket(buffer, buffer.length);
-                        //el socket escucha y recibe en el packet
-                        socketUDP.receive(packet);
-                        String message = new String(packet.getData(), 0, packet.getLength());
-                        client.addMessage(message.substring(0, message.indexOf(":") - 1), message.substring(message.indexOf(":")));
-
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-
-
-
+            while (true) {
+                // limpiamos el buffer
+                buffer = new byte[1024];
+                packet = new DatagramPacket(buffer, buffer.length);
+                // esperamos recibir un mensaje
+                socket.receive(packet);
+                String received = new String(packet.getData(), 0, packet.getLength());
+                String sender = received.substring(0, received.indexOf(":")).trim();
+                String message = received.substring(received.indexOf(":")+1).trim();
+                client.addMessage(sender, message);
             }
-        });
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
